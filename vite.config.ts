7b7debec6c4ae682dc6c932b88cd53e6c9d23c6c
Carlos Martinez <curlosmart@gmail.com>
2024-11-17 12:13:34 -0500

    Testing out another PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -72,6 +72,15 @@ export default defineConfig({
 					},
 				],
 			},
+			strategies: 'generateSW',
+			workbox: {
+				// Only precache these files - html should be excluded
+				globPatterns: ['**/*.{js,css}'],
+
+				// Don't fallback on document based (e.g. `/some-page`) requests
+				// Even though this says `null` by default, I had to set this specifically to `null` to make it work
+				navigateFallback: null,
+			},
 		}),
 		vike({}),
 	],
